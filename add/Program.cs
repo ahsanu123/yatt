1454fef5812c47c9d9fbd5bb8cc195fbb69313b3
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2025-09-12 13:05:35 +0700

    feat: basic aspnet core authentication with google work, now time to dive

--- add/Program.cs
+++ add/Program.cs
@@ -1,3 +1,7 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using YATT.Add.Constants;
@@ -7,6 +11,38 @@ namespace YATT.Add;
 
 public class Program
 {
+    private static async Task HandleOnRemoteFailure(RemoteFailureContext context)
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "text/html";
+        await context.Response.WriteAsync("<html><body>");
+        await context.Response.WriteAsync(
+            "A remote failure has occurred: <br>"
+                + context
+                    .Failure.Message.Split(Environment.NewLine)
+                    .Select(s => HtmlEncoder.Default.Encode(s) + "<br>")
+                    .Aggregate((s1, s2) => s1 + s2)
+        );
+
+        if (context.Properties != null)
+        {
+            await context.Response.WriteAsync("Properties:<br>");
+            foreach (var pair in context.Properties.Items)
+            {
+                await context.Response.WriteAsync(
+                    $"-{HtmlEncoder.Default.Encode(pair.Key)}={HtmlEncoder.Default.Encode(pair.Value)}<br>"
+                );
+            }
+        }
+
+        await context.Response.WriteAsync("<a href=\"/\">Home</a>");
+        await context.Response.WriteAsync("</body></html>");
+
+        // context.Response.Redirect("/error?FailureMessage=" + UrlEncoder.Default.Encode(context.Failure.Message));
+
+        context.HandleResponse();
+    }
+
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
@@ -26,9 +62,10 @@ public class Program
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
         builder
-            .Services.AddDefaultIdentity<IdentityUser>(options =>
-                options.SignIn.RequireConfirmedAccount = true
-            )
+            .Services.AddDefaultIdentity<IdentityUser>(option =>
+            {
+                option.SignIn.RequireConfirmedEmail = true;
+            })
             .AddEntityFrameworkStores<ApplicationDbContext>();
 
         // # Summary to setup external authentication with google
@@ -39,12 +76,17 @@ public class Program
         // - For local testing, use the default address https://localhost:{PORT}/signin-google, where the {PORT} placeholder is the app's port.
         //
         builder
-            .Services.AddAuthentication()
+            .Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie()
             .AddGoogle(option =>
             {
                 // csharpier-ignore-start
                 option.ClientId = configuration[GoogleAuthenticationConstant.AuthenticationClientId]!;
                 option.ClientSecret = configuration[GoogleAuthenticationConstant.AuthenticationClientId]!;
+                option.Events = new OAuthEvents()
+                {
+                    OnRemoteFailure = HandleOnRemoteFailure
+                };
                 // csharpier-ignore-end
             });
 
@@ -68,6 +110,7 @@ public class Program
 
         app.UseRouting();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapStaticAssets();
